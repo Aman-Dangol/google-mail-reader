639b0e5c80f6acd05fec639126913ffaeb83f8a7
Aman-Dangol <amandangol423@gmail.com>
2026-07-29 13:13:30 +0545

    feat: mail close button

--- apps/web/src/pages/Mails/components/mail-actions/mail-actions.tsx
+++ apps/web/src/pages/Mails/components/mail-actions/mail-actions.tsx
@@ -1,3 +1,21 @@
+import { Button } from "@src/components/button";
+import { MailContext } from "@src/utils/context/selected-mail-context";
+import { X } from "lucide-react";
+import { useContext } from "react";
+
 export const MailActions = () => {
-  return <section>Mail Actions</section>;
+  const { setSelectedMail } = useContext(MailContext);
+
+  const handleClose = () => {
+    setSelectedMail(undefined);
+  };
+  const CloseButton = (
+    <Button
+      onClick={handleClose}
+      className='size-6'>
+      <X className='stroke-bg-secondary size-4' />
+    </Button>
+  );
+
+  return <section className='flex justify-end p-2'>{CloseButton}</section>;
 };
