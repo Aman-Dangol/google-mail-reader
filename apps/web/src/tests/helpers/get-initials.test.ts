1eaf121c7c3797f50800278801e0a14bc42b5948
Aman-Dangol <amandangol423@gmail.com>
2026-08-17 12:52:13 +0545

    feat: added test for get initials

---
+++ apps/web/src/tests/helpers/get-initials.test.ts
@@ -0,0 +1,20 @@
+import { getInitials } from "@src/utils/helpers/get-initials";
+import { describe, expect, it } from "vitest";
+
+describe("retuns initials of a name", () => {
+  it("retuns empty string", () => {
+    const name = "";
+    expect(getInitials(name)).toBe("");
+  });
+
+  it("returns first and last name initals", () => {
+    const name = "Test User";
+
+    expect(getInitials(name)).toBe("TU");
+  });
+
+  it("returns only first name initial", () => {
+    const name = "Test";
+    expect(getInitials(name)).toBe("T");
+  });
+});
